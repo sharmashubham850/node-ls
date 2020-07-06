e9b988366aacfeabdcdfae1274b5ccefbee9f92a
Shubham Sharma <sharmashubham850@gmail.com>
2020-07-06 23:27:31 +0530

    file/directory identification

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 #!/usr/bin/env node
 const fs = require('fs');
+const util = require('util');
 
-fs.readdir(process.cwd(), (err, files) => {
+// Method 2
+// const lstat = util.promisify(fs.lstat);
+
+// Method 3
+const { lstat } = fs.promises;
+
+fs.readdir(process.cwd(), async (err, filenames) => {
     if (err) {
         console.log(err);
     }
 
-    for (let file of files) {
-        console.log(file);
+    const statPromises = filenames.map((filename) => {
+        return lstat(filename);
+    });
+
+    const allStats = await Promise.all(statPromises);
+
+    for (let i = 0; i < filenames.length; i++) {
+        console.log(filenames[i], allStats[i].isFile());
     }
 })
+
+// Method 1
+// function lstat(filename) {
+//     return new Promise((resolve, reject) => {
+//         fs.lstat(filename, (err, stats) => {
+//             if (err) reject(err);
+
+//             resolve(stats);
+//         })
+//     })
+// }
